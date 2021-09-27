bcb91dbba29a5c1d826e9ca3abc68e8fe6105b47
shivam212 <shivamsinha212@gmail.com>
2021-09-27 12:46:29 +0530

    added c++ runner

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import reqparse, Resource, Api
-from runner import coderunnerPython
+from runner import coderunnerPython, coderunnerCPP
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,7 +8,7 @@ parser = reqparse.RequestParser()
 parser.add_argument('code')
 parser.add_argument('input')
 
-class codeRunner(Resource):
+class codeRunnerPython(Resource):
     def get(self):
         return {"working":"working"}
     def post(self):
@@ -20,7 +20,20 @@ class codeRunner(Resource):
         output = coderunnerPython(input) 
         return {"args":args, "output":output[1], "compile": output[0]}
 
-api.add_resource(codeRunner,'/python')
+class codeRunnerCPP(Resource):
+    def get(self):
+        return {"working":"working"}
+    def post(self):
+        args = parser.parse_args()
+        code = args['code']
+        input = args['input']
+        with open('code_to_run.cpp', mode='w') as f:
+            f.write(code) 
+        output = coderunnerCPP(input) 
+        return {"args":args, "output":output[1], "compile": output[0]}
+ 
+api.add_resource(codeRunnerPython,'/python')
+api.add_resource(codeRunnerCPP,'/cpp')
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", debug=True)
--- runner.py
+++ runner.py
@@ -5,3 +5,13 @@ def coderunnerPython(inputString):
     if(len(runner.stderr)>0):
         return [0,runner.stderr.decode()]
     return [1,runner.stdout.decode()]  #the output from the runner is taken and decoded and returned
+
+def coderunnerCPP(inputString): 
+    runnerPrelim = subprocess.run(['g++', 'code_to_run.cpp'], stdout= subprocess.PIPE, stderr=subprocess.PIPE, input = inputString.encode()) #calls a process to compile the c++ code 
+    if(len(runnerPrelim.stderr)==0):
+        runner = subprocess.run(['./a.out'], stdout= subprocess.PIPE, stderr=subprocess.PIPE, input = inputString.encode())
+    else:
+        return [0,runnerPrelim.stderr.decode()]
+    if(len(runner.stderr)>0):
+        return [0,runner.stderr.decode()]
+    return [1,runner.stdout.decode()]  #the output from the runner is taken and decoded and returned
